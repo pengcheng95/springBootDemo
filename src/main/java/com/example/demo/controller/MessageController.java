c7ad75b3f782a5b475ec0d3d49c0a75942049ad7
Max Peng <pengcheng95@gmail.com>
2019-06-18 11:25:36 -0400

    create post request

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -2,16 +2,22 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.Message;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
 
-    @ResponseBody
+
     @GetMapping("/message")
     Message send() {
         return new Message("first message");
+    }
+
+    @PostMapping("/message")
+    Message echo (@RequestBody Message message) {
+        return message;
     }
 }
